403e83bb0ea8684ff96a2c08e840d792e79573f2
Chih Huai LIN <chihhuai.lin@student.vinci.be>
2020-10-31 19:08:19 +0100

    show current time

--- index.php
+++ index.php
@@ -6,6 +6,4 @@ $hours = $berlin_clock->init_hours();
 $minutes = $berlin_clock->init_minutes();
 $seconds = $berlin_clock->init_seconds();
 
-echo $berlin_clock->get_seconds($seconds);
-echo $berlin_clock->init_hours()+\n;
-echo $berlin_clock->init_minutes()+\n;
+echo $seconds +":"+$minutes+":"+"$hours"+\n;
