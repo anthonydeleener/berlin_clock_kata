fdd9a9f48616110029a64467956b8333fc9a66ea
Anthony <anthony.deleener@student.vinci.be>
2020-10-31 17:35:50 +0100

    berlin_clock advances

--- berlin_clock.js
+++ berlin_clock.js
@@ -1,1 +1,19 @@
-git test
+class berlin_clock{
+
+    constructor() {
+        let now = new Date();
+        let h = now.getHours();
+        let m = now.getMinutes();
+        let s = now.getSeconds();
+    }
+
+    get_seconds=()=>{
+            this.seconds = document.querySelector("#html_seconds");
+            seconds.textContent = "X";
+            if(s%2 === 0) {
+                return 0;
+                seconds.textContent = "S";
+            }
+            return 1;
+    }
+}
