a43f5f8d35e628dd60bd56c60f7afb07d2b4197e
Chih Huai LIN <chihhuai.lin@student.vinci.be>
2020-11-04 15:39:34 +0100

    derniere mise a jour

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 require_once('BerlinClock.php');
-$berlin_clock = new BerlinClock();
+$berlinClock = new BerlinClock();
 
-$hours = $berlin_clock->initHours();
-$minutes = $berlin_clock->initMinutes();
-$seconds = $berlin_clock->initSeconds();
+$hours = $berlinClock->initHours();
+$minutes = $berlinClock->initMinutes();
+$seconds = $berlinClock->initSeconds();
 
-echo $berlin_clock->translateSecond($seconds) . "<br>";
-echo $berlin_clock->translateHours($berlin_clock->getFiveHours($hours)) . "<br>";
-echo $berlin_clock->translateHours($berlin_clock->getHours($hours)) . "<br>";
-echo $berlin_clock->translateMinutes($berlin_clock->getFiveMinutes($minutes)) . "<br>";
-echo $berlin_clock->translateMinutes($berlin_clock->getMinutes($minutes)) . "<br>";
+echo $berlinClock->translateSecond($seconds) . "<br>";
+echo $berlinClock->translateHours($berlinClock->getFiveHours($hours)) . "<br>";
+echo $berlinClock->translateHours($berlinClock->getHours($hours)) . "<br>";
+echo $berlinClock->translateMinutes($berlinClock->getFiveMinutes($minutes)) . "<br>";
+echo $berlinClock->translateMinutes($berlinClock->getMinutes($minutes)) . "<br>";
